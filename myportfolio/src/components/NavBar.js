112eed5094de09c573da6043df507daeb1596236
carltaer <carlchristopher.taer@gmail.com>
2023-04-19 22:18:38 +0100

    configure navabar

--- myportfolio/src/components/NavBar.js
+++ myportfolio/src/components/NavBar.js
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react"
-import { Navbar, Container } from "react-bootstrap"
+import { Navbar, Container, Nav } from "react-bootstrap"
+import logo from "../assets/img/logo.svg"
+import navIcon1 from "../assets/img/nav-icon1.svg"
+import navIcon2 from "../assets/img/nav-icon2.svg"
+import navIcon3 from "../assets/img/nav-icon3.svg"
 
-export const NavBar = () =>{
+
+export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home')
   const [scrolled, setScrolled] = useState(false)
 
@@ -9,11 +14,11 @@ export const NavBar = () =>{
     const onScroll = () => {
       if (window.scrollY > 50) {
         setScrolled(true)
-      }else{
+      } else {
         setScrolled(false)
       }
     }
-
+    
     window.addEventListener("scroll", onScroll)
   }, [])
 
@@ -22,10 +27,10 @@ export const NavBar = () =>{
   }
   
   return (
-        <Navbar expand="lg" className={scroll ? "scrolled" : ""}>
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={""} alt="Logo"/>
+            <img src={logo} alt="Logo"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
@@ -38,9 +43,9 @@ export const NavBar = () =>{
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#"><img src={""} alt=""/></a>
-              <a href="#"><img src={""} alt=""/></a>
-              <a href="#"><img src={""} alt=""/></a>
+              <a href="#"><img src={navIcon1} alt=""/></a>
+              <a href="#"><img src={navIcon2} alt=""/></a>
+              <a href="#"><img src={navIcon3} alt=""/></a>
             </div>
             <button className="vvd" onClick={()=> console.log("connect")}><span>Let's Connect</span></button>
           </span> 
